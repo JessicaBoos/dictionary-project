a15f459eab73f37f514507103c212889368264ec
JessicaBoos <jessica.boos28@gmail.com>
2025-01-24 16:16:24 +0100

    added synonyms

--- src/Meanining.js
+++ src/Meanining.js
@@ -1,7 +1,7 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
@@ -9,9 +9,14 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
+
+              <strong>Example: </strong>
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
