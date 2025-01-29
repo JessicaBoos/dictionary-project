5f2e27b4a4d120dd56d7c80068d657b7b8424e54
JessicaBoos <jessica.boos28@gmail.com>
2025-01-29 17:36:40 +0100

    updated footer

--- src/App.js
+++ src/App.js
@@ -17,7 +17,23 @@ export default function App() {
         <Dictioanary defaultKeyword="Star" />
       </main>
       <footer className="text-center">
-        This Project was coded by Jessica Boos.
+        This Project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/jessica-boos-9282b8294/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jessica Boos
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://github.com/JessicaBoos/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on GitHub
+        </a>
+        .
       </footer>
     </div>
   );
